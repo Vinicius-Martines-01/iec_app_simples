2ca89eb7384c7c2822cb87660ba96abda0b5106a
Vinícius <viniciusmartines.estudos@gmail.com>
2026-05-13 13:23:33 -0300

    feat: migrando tudo para o backend

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 // Conexão com o Banco de Dados usando o nome do serviço no Docker Compose
 const pool = new Pool({
